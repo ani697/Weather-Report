7e403ed1ec69035e1234aa13d2fb81e02ce73bb6
Aniket Srivastava <aniket697@gmail.com>
2019-05-15 17:12:08 +0530

    Change in geocode and forecast url

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/3b7b603ba501f03d686df8e728cdba28/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,8 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
-
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1IjoiYW5pNjk3IiwiYSI6ImNqdGtnNDVkZzE0ZG00YWtpZHpuaHE2NWoifQ.kcmLwh_LamnIU3MJnnBwyA';
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
